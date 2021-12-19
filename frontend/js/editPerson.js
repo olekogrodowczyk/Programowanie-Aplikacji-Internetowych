d04f8c44179bdf6c491520903b9acfc20135855f
ogrodowczyko <ollo9998@gmail.com>
2021-12-19 21:48:29 +0100

    Init

--- frontend/js/editPerson.js
+++
@@ -1,8 +0,0 @@
-app.controller('EditPersonCtrl', [ '$uibModalInstance', 'options', function($uibModalInstance, options) {
-    let ctrl = this
-    ctrl.options = options
-
-    ctrl.submit = function(answer) { $uibModalInstance.close(answer) }
-    ctrl.cancel = function() { $uibModalInstance.dismiss(null) }
-
-}])
--- server.js
+++ server.js
@@ -1,74 +1,96 @@
-const http = require('http')
-const url = require('url')
-const nodestatic = require('node-static')
-const uuid = require('uuid')
-const cookies = require('cookies')
+const http = require("http");
+const url = require("url");
+const nodestatic = require("node-static");
+const uuid = require("uuid");
+const cookies = require("cookies");
 
-const lib = require('./lib')
-const person = require('./person')
-const db = require('./db')
-const auth = require('./auth')
-const example = require('./example')
-const deposit = require('./deposit')
+const lib = require("./lib");
+const person = require("./person");
+const db = require("./db");
+const auth = require("./auth");
+const example = require("./example");
+const deposit = require("./deposit");
 
-let server = http.createServer()
-let fileServer = new nodestatic.Server('./frontend');
+let server = http.createServer();
 
-server.on('request', function(req, res) {
-    let env = { req, res }
+server.on("request", function (req, res) {
+  let env = { req, res };
 
-    let appCookies = new cookies(req, res)
-    let session = appCookies.get('session')
-    let now = Date.now()
-    if(!session || !lib.sessions[session]) {
-        session = uuid.v4()
-        lib.sessions[session] = { from: req.connection.remoteAddress, created: now, touched: now }
-    } else {
-        lib.sessions[session].from = req.connection.remoteAddress
-        lib.sessions[session].touched = now
-    }
-    appCookies.set('session', session, { httpOnly: false })
-    env.session = session
+  let appCookies = new cookies(req, res);
+  let session = appCookies.get("session");
+  let now = Date.now();
+  if (!session || !lib.sessions[session]) {
+    session = uuid.v4();
+    lib.sessions[session] = {
+      from: req.connection.remoteAddress,
+      created: now,
+      touched: now,
+    };
+  } else {
+    lib.sessions[session].from = req.connection.remoteAddress;
+    lib.sessions[session].touched = now;
+  }
+  appCookies.set("session", session, { httpOnly: false });
+  env.session = session;
 
-    env.urlParsed = url.parse(req.url, true)
-    if(!env.urlParsed.query) env.urlParsed.query = {}
+  env.urlParsed = url.parse(req.url, true);
+  if (!env.urlParsed.query) env.urlParsed.query = {};
 
-    if(!lib.checkPermissions(req.method + ' ' + env.urlParsed.pathname, lib.sessions[session].roles)) {
-        lib.sendError(res, 403, 'permission denied')
-        return
-    }
+  if (
+    !lib.checkPermissions(
+      req.method + " " + env.urlParsed.pathname,
+      lib.sessions[session].roles
+    )
+  ) {
+    lib.sendError(res, 403, "permission denied");
+    return;
+  }
 
-    env.payload = ''
-    req.on('data', function(data) {
-        env.payload += data
-    }).on('end', function() {
-        try {
-            env.payload = env.payload ? JSON.parse(env.payload) : {}
-        }  catch(ex) {
-            console.error(req.method, env.urlParsed.pathname, JSON.stringify(env.urlParsed.query), 'ERROR PARSING:', env.payload)
-            lib.sendError(res, 400, 'parsing payload failed')
-            return
-        }
-        console.log(session, req.method, env.urlParsed.pathname, JSON.stringify(env.urlParsed.query), JSON.stringify(env.payload))
-        switch(env.urlParsed.pathname) {
-            case '/auth':
-                auth.handle(env)
-                break
-            case '/person': 
-                person.handle(env)
-                break
-            case '/deposit':
-                deposit.handle(env)
-                break
-            default:
-                fileServer.serve(req, res)
-        }
+  env.payload = "";
+  req
+    .on("data", function (data) {
+      env.payload += data;
     })
-})
+    .on("end", function () {
+      try {
+        env.payload = env.payload ? JSON.parse(env.payload) : {};
+      } catch (ex) {
+        console.error(
+          req.method,
+          env.urlParsed.pathname,
+          JSON.stringify(env.urlParsed.query),
+          "ERROR PARSING:",
+          env.payload
+        );
+        lib.sendError(res, 400, "parsing payload failed");
+        return;
+      }
+      console.log(
+        session,
+        req.method,
+        env.urlParsed.pathname,
+        JSON.stringify(env.urlParsed.query),
+        JSON.stringify(env.payload)
+      );
+      switch (env.urlParsed.pathname) {
+        case "/auth":
+          auth.handle(env);
+          break;
+        case "/person":
+          person.handle(env);
+          break;
+        case "/deposit":
+          deposit.handle(env);
+          break;
+        default:
+          fileServer.serve(req, res);
+      }
+    });
+});
 
-db.init(function() {
-    // for development only
-    example.initialize()
-    //
-    server.listen(7777)
-})
+db.init(function () {
+  // for development only
+  example.initialize();
+  //
+  server.listen(7777);
+});
