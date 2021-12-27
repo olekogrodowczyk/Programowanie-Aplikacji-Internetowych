5308daf5433c2573d2604d79823d1f35ef1e74f5
ogrodowczyko <ollo9998@gmail.com>
2021-12-27 05:35:04 +0100

    Update response in transactions

--- transaction.js
+++ transaction.js
@@ -2,34 +2,62 @@ const db = require("./db");
 const lib = require("./lib");
 
 const person = (module.exports = {
-  handle: function (env) {
+  handle: async function (env) {
     let q = env.urlParsed.query.q ? env.urlParsed.query.q : "";
 
-    const sendAllTransactions = function (q = "") {
-      db.transactions.find({}).toArray(function (err, transactions) {
-        if (!err) {
-          lib.sendJson(env.res, transactions);
-        } else {
-          lib.sendError(env.res, 400, "persons.aggregate() failed " + err);
-        }
-      });
+    const sendAllTransactions = async function (q = "") {
+      await db.transactions
+        .find({})
+        .toArray(async function (err, transactions) {
+          if (!err) {
+            let newArray = await Promise.all(
+              transactions.map(async function (obj) {
+                const recipient = await db.persons.findOne({
+                  _id: obj.recipient,
+                });
+                return {
+                  _id: obj._id,
+                  recipient: obj.recipient,
+                  amount: obj.amount,
+                  when: obj.when,
+                  recipientName: recipient.firstName + " " + recipient.lastName,
+                };
+              })
+            );
+            console.log(newArray);
+            lib.sendJson(env.res, newArray);
+          } else {
+            lib.sendError(env.res, 400, "persons.aggregate() failed " + err);
+          }
+        });
     };
 
     switch (env.req.method) {
       case "GET":
         recipientId = db.ObjectId(env.urlParsed.query.recipientId);
         if (recipientId) {
+          const recipient = await db.persons.findOne({ _id: recipientId });
           const transactions = db.transactions
             .find({ recipient: recipientId })
             .toArray(function (err, transactions) {
               if (!err) {
-                lib.sendJson(env.res, transactions);
+                let newArray = transactions.map(function (obj) {
+                  return {
+                    _id: obj._id,
+                    recipient: obj.recipient,
+                    amount: obj.amount,
+                    when: obj.when,
+                    recipientName:
+                      recipient.firstName + " " + recipient.lastName,
+                  };
+                });
+                lib.sendJson(env.res, newArray);
               } else {
                 lib.sendError(env.res, 400, "transactions failed " + err);
               }
             });
         } else {
-          sendAllTransactions(q);
+          await sendAllTransactions(q);
         }
         break;
       default:
