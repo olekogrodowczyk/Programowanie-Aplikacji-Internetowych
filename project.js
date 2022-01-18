180eeb71ff516ff91f1c5d0378b507eb1b2437b4
ogrodowczyko <ollo9998@gmail.com>
2022-01-18 15:13:53 +0100

    Fix adding projects in backend

--- project.js
+++ project.js
@@ -6,32 +6,22 @@ const person = (module.exports = {
     let q = env.urlParsed.query.q ? env.urlParsed.query.q : "";
 
     validateData = (projectData) => {
-      return projectData.manager && projectData.name ? true : false;
-    };
-
-    validateExistance = async (managerId) => {
-      let result = await db.users.findOne({
-        _id: db.ObjectId(managerId),
-      });
-      return result ? true : false;
+      return projectData.name ? true : false;
     };
 
     if (env.req.method == "POST") {
-      validationResult = validate(env.payload);
+      validationResult = validateData(env.payload);
       if (!validationResult) {
         lib.sendError(env.res, 400, "Invalid data");
-        return;
-      }
-      let managerExists = await validateExistance(env.payload.managerId);
-      if (!userExists) {
-        lib.sendError(env.res, 400, "Defined manager doesn't exist");
         return;
       }
     }
 
     switch (env.req.method) {
       case "POST":
-        db.transactions.insertOne(depositData, function (err, result) {
+        let depositData = env.payload;
+        depositData.creator = lib.sessions[env.session]._id;
+        db.projects.insertOne(depositData, function (err, result) {
           if (!err) {
             lib.sendJson(env.res, depositData);
           } else {
